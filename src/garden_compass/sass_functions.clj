77852c60670dd99c61e168fed43072f361250c31
Dan Neumann <danrodneu@gmail.com>
2013-10-18 11:41:44 -0500

    Remove sass-functions

--- src/garden_compass/sass_functions.clj
+++
@@ -1,8 +0,0 @@
-(ns garden-compass.sass-functions)
-
-;; http://sass-lang.com/documentation/Sass/Script/Functions.html#lightness-instance_method
-
-(defn lighten
-  "Returns lightness component of color.
-   - `color` should be integer 0 to 100 (represents percent)."
-  [color])
